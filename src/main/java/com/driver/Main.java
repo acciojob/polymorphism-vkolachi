0cadf26a6877a0c2ac397fdf72d65ba286f11769
vkola <vkolachi@gmail.com>
2023-08-19 13:41:19 +0530

    1st attemp

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    public class Product{
+    public static class Product{
         public int product(int x, int y) {
         return x*y;
         }
@@ -14,7 +14,7 @@ public class Main {
 
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Product p=new  Product();
         p.product(10,5);
         p.product(10,3,5);
